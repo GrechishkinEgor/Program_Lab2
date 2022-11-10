d0e68036e1d8c6b4081e740f4910efdc263e4e80
Гречишкин Егор <egor.grechishkin@bk.ru>
2022-11-11 01:06:35 +0700

    7 лабораторная. В main() добавлен код с контейнером vector<Product*>, сортировкой контейнера, поиском в контейнере

--- Program.cpp
+++ Program.cpp
@@ -6,6 +6,22 @@
 #include "Printer.h"
 #include "Keyboard.h"
 #include "Monitor.h"
+#include <vector>
+#include <algorithm>
+
+bool SortByNameAfterCompany(Product* obj1, Product* obj2)
+{
+    if (obj1->GetCompany() < obj2->GetCompany())
+        return true;
+    else
+        if (obj1->GetCompany() == obj2->GetCompany())
+            if (obj1->GetName() < obj2->GetName())
+                return true;
+            else
+                return false;
+        else
+            return false;
+}
 
 int main()
 {
@@ -55,6 +71,65 @@ int main()
     std::cout << *SecondPointerOnProduct;
     printf("-----\n");
     std::cout << *((Monitor*)SecondPointerOnProduct);
-    printf("-----'n");
+    printf("-----\n");
+
+    /*lab7*/
+    std::vector<Product*> ProductLine;
+    for (int i = 0; i < 4; i++)
+    {
+        std::string CurrentName;
+        std::cin >> CurrentName;
+        ProductLine.push_back(new Product(CurrentName, std::string("Enter Inc") + std::string(1, 'a' + (char)i % 2), (i + 1) * 50000, 20 - i));
+    }
+    try
+    {
+        ProductLine.insert(ProductLine.begin(), new Monitor("MonitorLikeProduct"));
+    }
+    catch (...)
+    {
+        std::cout << "Не удалось добавить монитор в массив" << std::endl;
+    }
+
+    for (int i = 0; i < ProductLine.size(); i++)
+        std::cout << ProductLine[i]->GetName() << " - " << ProductLine[i]->GetCompany() << " - " << ProductLine[i]->GetPrice() << " - " << ProductLine[i]->GetCount() << std::endl;
+    std::cout << std::endl;
+
+
+    /*Сортировка по названиям товаров (алфавитный порядок)*/
+    std::sort(ProductLine.begin(), ProductLine.end(), [](Product* obj1, Product* obj2) -> bool {return obj1->GetName() < obj2->GetName(); });
+    for (int i = 0; i < ProductLine.size(); i++)
+        std::cout << ProductLine[i]->GetName() << " - " << ProductLine[i]->GetCompany() << " - " << ProductLine[i]->GetPrice() << " - " << ProductLine[i]->GetCount() << std::endl;
+    std::cout << std::endl;
+    
+    /*Сортировка по компанию-производителю, а затем по названию товара*/
+    std::sort(ProductLine.begin(), ProductLine.end(), SortByNameAfterCompany);
+    for (int i = 0; i < ProductLine.size(); i++)
+        std::cout << ProductLine[i]->GetName() << " - " << ProductLine[i]->GetCompany() << " - " << ProductLine[i]->GetPrice() << " - " << ProductLine[i]->GetCount() << std::endl;
+    std::cout << std::endl;
+
+    /*Сортировка по цене товаров (по убыванию)*/
+    std::sort(ProductLine.begin(), ProductLine.end(), [](Product* obj1, Product* obj2) -> bool {return obj1->GetPrice() > obj2->GetPrice(); });
+    for (int i = 0; i < ProductLine.size(); i++)
+        std::cout << ProductLine[i]->GetName() << " - " << ProductLine[i]->GetCompany() << " - " << ProductLine[i]->GetPrice() << " - " << ProductLine[i]->GetCount() << std::endl;
+    std::cout << std::endl;
+
+    /*Сортировка по количеству товаров определенного типа (по убыванию)*/
+    std::sort(ProductLine.begin(), ProductLine.end(), [](Product* obj1, Product* obj2) -> bool {return obj1->GetCount() > obj2->GetCount(); });
+    for (int i = 0; i < ProductLine.size(); i++)
+        std::cout << ProductLine[i]->GetName() << " - " << ProductLine[i]->GetCompany() << " - " << ProductLine[i]->GetPrice() << " - " << ProductLine[i]->GetCount() << std::endl;
+    std::cout << std::endl;
+
+
+    /*Поиск по названию продукта*/
+    std::string SearchName;
+    std::cin >> SearchName;
+    std::vector<Product*>::iterator SearchResult = std::find_if(ProductLine.begin(), ProductLine.end(), [SearchName](Product* obj) {return obj->GetName() == SearchName; });
+    if (SearchResult != ProductLine.end())
+        std::cout << **SearchResult;
+    /*Найти ещё один подходящий элемент*/
+    SearchResult++;
+    std::vector<Product*>::iterator SearchResult2 = std::find_if(SearchResult, ProductLine.end(), [SearchName](Product* obj) {return obj->GetName() == SearchName; });
+    if (SearchResult2 != ProductLine.end())
+        std::cout << **SearchResult2;
     return 0;
 }
